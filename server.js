618df466dc8d58c135a69fb08665234cde2e6b2d
michael swigat <sherlockholmes.swigart92@gmail.com>
2021-04-20 19:40:39 -0500

    port 2

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(
 // routes
 app.use(require("./routes/api.js"));
 
-// app.listen(PORT, () => {
-//   console.log(`App running on port ${PORT}!`);
-// });
-port = process.env.PORT || 80
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+port = process.env.PORT || 3000
